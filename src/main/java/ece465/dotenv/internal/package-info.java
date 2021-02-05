f1927129499484aa3d66ece25edd000b13668993
Victoooooor <zhang20@cooper.edu>
2021-02-06 07:40:33 +0800

    standing up

--- src/main/java/ece465/dotenv/internal/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Provides a set of internal helpers for dotenv (not for external use).
- */
-package ece465.dotenv.internal;
--- src/main/java/ece465/dotenv/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Provides a system to manage environment variables using a .env file.
- */
-package ece465.dotenv;
