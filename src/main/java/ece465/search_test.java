19230976d4c6da5268f872ee4e1dd17c76a97fb4
Victoooooor <zhang20@cooper.edu>
2021-02-06 10:05:17 +0800

    Final-2

---
+++ src/main/java/ece465/search_test.java
@@ -0,0 +1,29 @@
+package ece465;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.Scanner;
+
+import ece465.handler.single.retrieve;
+import ece465.util.DBconnection;
+
+public class search_test {
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a search word: ");
+        String searchWord = sc.nextLine();
+        System.out.print("Enter number of threads: ");
+        final int nThreads = sc.nextInt();
+
+        LocalTime start = LocalTime.now();
+
+        DBconnection DB_con = new DBconnection();
+        retrieve RT = new retrieve(DB_con, nThreads);
+        RT.startSearch(searchWord);
+
+        LocalTime end = LocalTime.now();
+
+        System.out.println("Run time: " + Duration.between(start, end).toMillis() + " ms");
+
+    }
+}
