66e002db7664914baeff3958282a0cc0f20f2939
Victoooooor <zhang20@cooper.edu>
2021-02-20 09:55:05 +0800

    .5 wulala

--- src/main/java/ece465/node/peerlist.java
+++ src/main/java/ece465/node/peerlist.java
@@ -1,10 +1,12 @@
 package ece465.node;
 
 import java.util.ArrayList;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class peerlist {//to implement node to node connection, consensus, central node lookup
     private ArrayList<peer> nodelist;
-    public class peer{
+
+    private class peer{
         private String ip;
         private Integer port;
         public peer(String ip, Integer port){
@@ -16,6 +18,28 @@ public class peerlist {//to implement node to node connection, consensus, centra
         }
         public Integer getport(){
             return this.port;
+    }
+    }
+
+    private class subclient implements Runnable{
+        ConcurrentLinkedQueue<peer> con_list;
+        public subclient(ConcurrentLinkedQueue<peer> con_list){
+            this.con_list=con_list;
         }
+        @Override
+        public void run() {
+
+        }
+    }
+
+    public void register(String ip, Integer port){
+        nodelist.add(new peer(ip,port));
+    }
+    public void broadcast (String message){
+        ConcurrentLinkedQueue<peer> con_list=new ConcurrentLinkedQueue<>(nodelist);
+
+        nodelist.forEach(node->{
+
+        });
     }
 }
