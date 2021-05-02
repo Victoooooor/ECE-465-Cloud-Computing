31bca2e229200f074418dbd96d7d08a4a8f40c81
Allister Liu <allisterakun@gmail.com>
2021-05-02 09:15:51 -0700

    ui

--- src/main/java/ece465/util/fileInfo.java
+++ src/main/java/ece465/util/fileInfo.java
@@ -1,5 +1,5 @@
 package ece465.util;
-
+/*
 public class fileInfo {
     private final int fid;
     private final String filename;
@@ -22,4 +22,53 @@ public class fileInfo {
 
     public String getHash() { return hash; }
 }
+*/
+public class fileInfo{
+    private final int fid;
+    private final String filename;
+    private String hash;
+    private String IP;
+    private int port;
 
+    public fileInfo(int fid, String filename){
+        this.fid = fid;
+        this.filename = filename;
+    }
+
+    public fileInfo(int fid, String filename, String hash){
+        this.fid = fid;
+        this.filename = filename;
+        this.hash = hash;
+    }
+
+    public fileInfo(int fid, String filename, String IP, int port){
+        this.fid = fid;
+        this.filename = filename;
+        this.IP = IP;
+        this.port = port;
+    }
+
+    public fileInfo(int fid, String filename, String hash, String IP, int port){
+        this.fid = fid;
+        this.filename = filename;
+        this.hash = hash;
+        this.IP = IP;
+        this.port = port;
+    }
+
+    public void setIP(String IP){ this.IP = IP; }
+
+    public void setPort(int port){ this.port = port; }
+
+    public void setHash(String hash){ this.hash = hash; }
+
+    public int getFid() { return fid; }
+
+    public String getFilename() { return filename; }
+
+    public String getHash() { return hash; }
+
+    public String getIP() { return IP; }
+
+    public int getPort() { return port; }
+}
